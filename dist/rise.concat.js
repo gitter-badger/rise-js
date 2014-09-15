7cecbb2bb61d5aee53bddcb8547cb61747d616fa
Eugene Obrezkov <ghaiklor@gmail.com>
2014-09-15 22:38:21 +0300

    Update build

--- dist/rise.concat.js
+++ dist/rise.concat.js
@@ -15,10 +15,10 @@
      */
     var defaultConfig = {
         canvas: {
-            width: 640,
-            height: 480,
+            width: 600,
+            height: 500,
             css: {
-                outline: '1px solid black',
+                boxShadow: '0px 0px 1px 1px #000000',
                 position: 'relative'
             }
         }
@@ -285,6 +285,56 @@
 
     global.Rise = Rise;
 
+})(this);
+(function(global) {
+    'use strict';
+
+    var services = {};
+
+    global.Rise.DI = {
+        /**
+         * Get service by name
+         * @param  {String} name Service name
+         * @return {Mixed}       Returns service if exists
+         */
+        get: function(name) {
+            return services[name];
+        },
+
+        /**
+         * Register service by name
+         * @param  {String} key   Service name
+         * @param  {Mixed} value  Value of service
+         * @return {Rise.DI}      Returns instance of Rise.DI
+         */
+        register: function(key, value) {
+            services[key] = value;
+            return this;
+        },
+
+        /**
+         * Resolve dependencies and call function
+         * @param  {Array} deps         Array with dependencies
+         * @param  {Function} callback  Function which applies dependencies
+         * @param  {Object} scope       Function context
+         * @return {Rise.DI}            Returns Rise.DI
+         */
+        resolve: function(dependencies, callback, scope) {
+            var args = [],
+                service;
+
+            for (var i = 0; i < dependencies.length; i++) {
+                service = this.get(dependencies[i]);
+                if (service) {
+                    args.push(service);
+                } else {
+                    throw new Error('Dependency not resolved -> ' + dependencies[i]);
+                }
+            }
+
+            callback.apply(scope || {}, args.concat(Array.prototype.slice.call(arguments, 0)));
+        }
+    };
 })(this);
 (function(global) {
     'use strict';
