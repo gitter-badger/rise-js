6e8b24a2efe03427dd1a7a5818821cf203501d76
Eugene Obrezkov <ghaiklor@MacBook-Pro-ghaiklor.local>
2014-11-09 15:39:44 +0200

    Remove all gulp tasks. Need to rewrite.

--- tasks/default.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    "use strict";
-
-    module.exports = function (gulp) {
-        gulp.task('default', ['webserver']);
-    };
-}());
--- tasks/watch.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    "use strict";
-
-    module.exports = function (gulp, config) {
-        gulp.task('watch', function () {
-            gulp.watch(config.sourceMap, ['build-js']);
-        });
-    };
-}());
