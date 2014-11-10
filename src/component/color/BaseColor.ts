8e53187775219977e660c6bd4eff1d434df9c593
ghaiklor <ghaiklor@gmail.com>
2014-11-10 20:25:12 +0200

    Continue working

---
+++ src/component/color/BaseColor.ts
@@ -0,0 +1,10 @@
+module Rise.Color {
+    export class BaseColor implements Rise.Color.IRGBColor {
+        _r:number;
+        _g:number;
+        _b:number;
+
+        constructor() {
+        }
+    }
+}
